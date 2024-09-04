08b8ea0f55c193cba9c58c2cb0e27ba0c789933b
Shaza Tamer <shazatamer2020@gmail.com>
2024-09-04 23:31:10 +0300

    auth

---
+++ database/migrations/2024_09_04_143821_create_user_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('user', function (Blueprint $table) {
+            $table->id();
+            $table->string('name',30);
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->bigInteger('SSN')->unique();
+            $table->string('phone')->unique()->nullable();
+            $table->string('image')->nullable();
+            $table->enum('gender', ['male', 'female', 'other'])->nullable();
+            $table->date('DOB')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('user');
+    }
+};
---
+++ database/migrations/2024_09_04_143907_create_lab_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('lab', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->bigInteger('lab_id')->unique();
+            $table->string('phone')->unique()->nullable();
+            $table->string('address')->nullable();
+            $table->string('loc')->nullable();
+            $table->dateTimeTz('workinghr');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('lab');
+    }
+};
